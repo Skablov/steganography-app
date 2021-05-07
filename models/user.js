307df99cd1aa80b63730806f9ca98f449c6b8290
Dan9 <danilaskablov@list.ru>
2021-05-07 11:10:00 +0300

    added mongoose schema User

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    login: {type: String, required: true},
+    psw: {type: String, required: true}
+});
+
+module.exports = mongoose.model('User', User);
